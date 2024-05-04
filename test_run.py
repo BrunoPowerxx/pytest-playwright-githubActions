51143cce5cb187167556ad558bff175b3b098663
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-04 04:28:58 +0200

    Update test_run.py print events

--- test_run.py
+++ test_run.py
@@ -20,21 +20,24 @@ def test_plr1():
         events2 = context2.query_selector_all('.plr_1')
         events3 = context3.query_selector_all('.plr_1')
         events4 = context4.query_selector_all('.plr_1')
-        
-        event1 = events1[0]  # Fixed variable names
-        event2 = events2[1]
-        event3 = events3[2]
-        event4 = events4[3]
+        print(events1)
+        print(events2)
+        print(events3)
+        print(events4)
+        #event1 = events1[0]  # Fixed variable names
+        #event2 = events2[1]
+        #event3 = events3[2]
+        #event4 = events4[3]
 
-        c1 = event1.inner_text  # Removed parentheses
-        c2 = event2.inner_text
-        c3 = event3.inner_text
+       # c1 = event1.inner_text  # Removed parentheses
+       # c2 = event2.inner_text
+       # c3 = event3.inner_text
         c4 = event4.inner_text
         
-        print(c1)
-        print(c2)
-        print(c3)
-        print(c4)
+      #  print(c1)
+       # print(c2)
+     #   print(c3)
+     #   print(c4)
         
         time.sleep(5)
         browser.close()
