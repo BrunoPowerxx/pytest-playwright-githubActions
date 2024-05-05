99351eabff85f8cc149e71d56dc312e6cb1d10d6
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-05 20:35:09 +0200

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -1,43 +1,33 @@
-import pytest
-import time  # Import the time module
 from playwright.sync_api import sync_playwright
+import time
 
-def test_plr1():
+
+def test_web1():
     with sync_playwright() as p:
         browser = p.chromium.launch()
-        context = browser.new_context()
-        context1 = context.new_page()
-        context2 = context.new_page()
-        context3 = context.new_page()
-        context4 = context.new_page()
-         
-        context1.goto('https://supabets.com/')
-        context2.goto('https://supabets.com')
-        context3.goto('https://supabets.com')
-        context4.goto('https://supabets.com')
-        
-        events1 = context1.query_selector_all('.plr_1')  # Fixed variable names
-        events2 = context2.query_selector_all('.plr_1')
-        events3 = context3.query_selector_all('.plr_1')
-        events4 = context4.query_selector_all('.plr_1')
-        print(events1)
-        print(events2)
-        print(events3)
-        print(events4)
-        #event1 = events1[0]  # Fixed variable names
-        #event2 = events2[1]
-        #event3 = events3[2]
-        #event4 = events4[3]
+        page = browser.new_page()
+        page.goto("https://supabets.co.za")
 
-       # c1 = event1.inner_text  # Removed parentheses
-       # c2 = event2.inner_text
-       # c3 = event3.inner_text
-      #  c4 = event4.inner_text
-        
-      #  print(c1)
-       # print(c2)
-     #   print(c3)
-     #   print(c4)
-        
+        # Wait for the soccer tab to appear and click on it
+        page.wait_for_selector('div[title="SOCCER"]').click()
         time.sleep(5)
+        # Wait for the subEvents to load and count the number of games
+        sub_events = page.query_selector_all('.subEvent')
+        home_teams = page.query_selector_all('.plr_1')
+        team_names =  [home_team.inner_text() for home_team in home_teams[:3]]
+        game_one = team_names[0]
+        game_two = team_names[1]
+        game_three = team_names[2]
+        
+        odds = page.query_selector_all('.oddValue')
+        odds_values = [odd.inner_text() for odd in odds[:3]]  # Extracting only the first three odds
+        odd_home = odds_values[0]
+        odd_draw = odds_values[1]
+        odd_away = odds_values[2]
+
+        print(game_one)
+        print(game_two)
+        print(game_three)
+        print(" ")
+        print("yeah motherfucker") 
         browser.close()
