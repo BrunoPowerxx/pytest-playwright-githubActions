5dde6d7d866e0ec3058e2d7607e5e94c6ac8d45c
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-06 06:55:04 +0200

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -4,30 +4,52 @@ import time
 
 def test_web1():
     with sync_playwright() as p:
+        
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://supabets.co.za")
-
-        # Wait for the soccer tab to appear and click on it
         page.wait_for_selector('div[title="SOCCER"]').click()
         time.sleep(5)
-        # Wait for the subEvents to load and count the number of games
-        sub_events = page.query_selector_all('.subEvent')
-        home_teams = page.query_selector_all('.plr_1')
-        team_names =  [home_team.inner_text() for home_team in home_teams[:3]]
-        game_one = team_names[0]
-        game_two = team_names[1]
-        game_three = team_names[2]
+
         
+        sub_events = page.query_selector_all('.subEvent')
+
+        home_teams = page.query_selector_all('.plr_1')
+        away_teams = page.query_selector_all('.plr_2')
+
         odds = page.query_selector_all('.oddValue')
+
+
+
+        home_names =  [home_team.inner_text() for home_team in home_teams[:3]]
+        away_names =  [away_team.inner_text() for away_team in away_teams[:3]]
+
         odds_values = [odd.inner_text() for odd in odds[:3]]  # Extracting only the first three odds
+
+
+
+        home_one = home_names[0]
+        home_two = home_names[1]
+        home_three = home_names[2]
+
+        away_one = away_names[0]
+        away_two = away_names[1]
+        away_three = away_names[2]
+
         odd_home = odds_values[0]
         odd_draw = odds_values[1]
         odd_away = odds_values[2]
 
-        print(game_one)
-        print(game_two)
-        print(game_three)
+
+
+        print(home_one)
+        print(home_two)
+        print(home_three)
+        print(away_one)
+        print(away_two)
+        print(away_three)
+
         print(" ")
-        print("yeah motherfucker") 
+        print("yeah motherfucker")
+
         browser.close()
