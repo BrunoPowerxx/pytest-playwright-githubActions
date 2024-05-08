7ca170ec2e9041bead0eb2b15f7ccb71e503764e
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-08 12:54:12 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -7,11 +7,11 @@ def test_web1():
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://supabets.co.za")
-        page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.wait_for_timeout(10000)  # Wait for 3 seconds
 
     # Click on the soccer option
         page.wait_for_selector('div[title="SOCCER"]').click()
-        page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.wait_for_timeout(10000)  # Wait for 3 seconds
 
     # Get and print home team text content
         home_team = page.query_selector('.plr_1')
@@ -19,7 +19,7 @@ def test_web1():
 
     # Click on the home team
         home_team.click()
-        page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.wait_for_timeout(10000)  # Wait for 3 seconds
 
     # Wait for the back button to appear
         page.wait_for_selector('span.btnBack')
@@ -27,7 +27,7 @@ def test_web1():
 
     # Click on the back button
         page.click('span.btnBack')
-        page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.wait_for_timeout(10000)  # Wait for 3 seconds
 
     # Print content of the soccer option button again
         print("Content of the soccer option button:", page.query_selector('div[title="SOCCER"]').inner_text())
