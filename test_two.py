c661f3f5d785d24349cda5ae2fbe1a53f656e5a3
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-08 12:51:33 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -1,36 +1,33 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-def browser():
+def test_web1():
     with sync_playwright() as p:
+        
         browser = p.chromium.launch()
-        yield browser
-        browser.close()
-
-def test_supabets(browser):
-    page = browser.new_page()
-    page.goto("https://www.supabets.co.za")
-    page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page = browser.new_page()
+        page.goto("https://supabets.co.za")
+        page.wait_for_timeout(3000)  # Wait for 3 seconds
 
     # Click on the soccer option
-    page.wait_for_selector('div[title="SOCCER"]').click()
-    page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.wait_for_selector('div[title="SOCCER"]').click()
+        page.wait_for_timeout(3000)  # Wait for 3 seconds
 
     # Get and print home team text content
-    home_team = page.query_selector('.plr_1')
-    print("Home Team:", home_team.inner_text())
+        home_team = page.query_selector('.plr_1')
+        print("Home Team:", home_team.inner_text())
 
     # Click on the home team
-    home_team.click()
-    page.wait_for_timeout(3000)  # Wait for 3 seconds
+        home_team.click()
+        page.wait_for_timeout(3000)  # Wait for 3 seconds
 
     # Wait for the back button to appear
-    page.wait_for_selector('span.btnBack')
-    print("Back button text:", page.query_selector('span.btnBack').inner_text())
+        page.wait_for_selector('span.btnBack')
+        print("Back button text:", page.query_selector('span.btnBack').inner_text())
 
     # Click on the back button
-    page.click('span.btnBack')
-    page.wait_for_timeout(3000)  # Wait for 3 seconds
+        page.click('span.btnBack')
+        page.wait_for_timeout(3000)  # Wait for 3 seconds
 
     # Print content of the soccer option button again
-    print("Content of the soccer option button:", page.query_selector('div[title="SOCCER"]').inner_text())
+        print("Content of the soccer option button:", page.query_selector('div[title="SOCCER"]').inner_text())
