48df770f61dd3d759feba96731c05f74b44699a7
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-05-09 04:06:46 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import time
 
 def test_web1():
     with sync_playwright() as p:
@@ -7,11 +8,11 @@ def test_web1():
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://supabets.co.za")
-        page.wait_for_timeout(10000)  # Wait for 3 seconds
+        time.sleep(5)  # Wait for 5 seconds
 
     # Click on the soccer option
         page.wait_for_selector('div[title="SOCCER"]').click()
-        page.wait_for_timeout(10000)  # Wait for 3 seconds
+        time.sleep(5)  # Wait for 5 seconds
 
     # Get and print home team text content
         home_team = page.query_selector('.plr_1')
@@ -19,7 +20,7 @@ def test_web1():
 
     # Click on the home team
         home_team.click()
-        page.wait_for_timeout(10000)  # Wait for 3 seconds
+        time.sleep(5)  # Wait for 5 seconds
 
     # Wait for the back button to appear
         page.wait_for_selector('span.btnBack')
@@ -27,7 +28,7 @@ def test_web1():
 
     # Click on the back button
         page.click('span.btnBack')
-        page.wait_for_timeout(10000)  # Wait for 3 seconds
+        time.sleep(5)  # Wait for 5 seconds
 
     # Print content of the soccer option button again
         print("Content of the soccer option button:", page.query_selector('div[title="SOCCER"]').inner_text())
