ecc9bbee378ff76c7235a273cc773f4d40a77017
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-06-24 12:18:07 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -6,19 +6,20 @@ event_title = "//div[@data-eventtile='Brazil']"
 types = "//div[ contains(@data-translate-market)]"
 values = "//div[ contains(@data-pd)]"
 
-def test_web1(): with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto(bw_basket)
-    time.sleep(5)
-    page.locator(event_tile).click
-    time.sleep(5)
-    odd_t = page.query_selector(types)
-    odd_v = page.query_selector(values)
-    type = odd_t.inner_text()
-    value = odd_v.inner_text()
-    print(type)
-    print(value)
-    print("yeah motherfucker!!")
+def test_web1():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(bw_basket)
+        time.sleep(5)
+        page.locator(event_tile).click
+        time.sleep(5)
+        odd_t = page.query_selector(types)
+        odd_v = page.query_selector(values)
+        type = odd_t.inner_text()
+        value = odd_v.inner_text()
+        print(type)
+        print(value)
+        print("yeah motherfucker!!")
 
-    browser.close()
+        browser.close()
