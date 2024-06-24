a3ec14ffbb4bbc3ceb6b9a14e2fe910399779dc9
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-06-24 11:57:20 +0200

    Update pest_two.py

--- pest_two.py
+++ pest_two.py
@@ -1,21 +1,24 @@
 from playwright.sync_api import sync_playwright
 import time
-from thefuzz import fuzz
-from thefuzz import process
 
-def test_web1():
-    with sync_playwright() as p:
-        
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("https://betway.co.za/sport/basketball")
-        
-        time.sleep(5)
+bw_basket = "https://betway.co.za/sport/basketball"
+event_title = "//div[@data-eventtile='Brazil']"
+types = "//div[ contains(@data-translate-market)]"
+values = "//div[ contains(@data-pd)]"
 
-        
-        
-        
-        print(" ")
-        print("yeah motherfucker")
+def test_web1(): with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(bw_basket)
+    time.sleep(5)
+    page.locator(event_tile).click
+    time.sleep(5)
+    odd_t = page.query_selector(types)
+    odd_v = page.query_selector(values)
+    type = odd_t.inner_text()
+    value = odd_v.inner_text()
+    print(type)
+    print(value)
+    print("yeah motherfucker")
 
-        browser.close()
+    browser.close()
