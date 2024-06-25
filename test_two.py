68b0c59aaf776bcd17d57a528d885857bfb70fcc
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-06-25 10:18:24 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -15,29 +15,30 @@ event = "//label[@data-translate-type='event' and @data-translate-set='Basketbal
 
     #Basketball Highlights
 qtrs = "//span[@data-translate-key='Quarters']"
+    #1st Quarter
 qoe_1 = "//span[@data-translate-key='1StQuarterOddEven']"
+    #1st Quarter - Odd
 qoe1_ot = "//span[@data-translate-market='1st Quarter - Odd/Even' and @data-translate-key='Odd']"
-qoe1_ov = "//span[@data-translate-key='1StQuarterOddEven']"
-     #   qoe1_et =
-      #  qoe1_ev =
-      #  qoe1_ov =
-      #  qoe1_et =
-      #  qoe1_ev =
-qoe_2 = "//span[@data-translate-key='2NdQuarterOddEven']"
-    #    qoe2_ot =
-     #   qoe2_ov =
-      #  qoe2_et =
-       # qoe2_ev =
-qoe_3 = "//span[@data-translate-key='3RQuarterOddEven']"
-    #    qoe3_ot =
-    #    qoe3_ov =
-    #    qoe3_et =
-    #    qoe3_ev =
-qoe_4 = "//span[@data-translate-key='4ThQuarterOddEven']"
-     #   qoe4_ot =
-     #   qoe4_ov =
-    #  qoe4_et =
-     #   qoe4_ev =
+qoe1_ov = "div.outcome-title.doublechance > span[data-translate-market='1st Quarter - Odd/Even'][data-translate-key='Odd'] ~ div.outcome-pricedecimal"
+    #1st Quarter  - Even
+qoe1_et = "//span[@data-translate-market='1st Quarter - Odd/Even' and @data-translate-key='Odd']"
+qoe_1_ev = "div.outcome-title.doublechance > span[data-translate-market='1st Quarter - Odd/Even'][data-translate-key='Even'] ~ div.outcome-pricedecimal"
+
+#    qoe_2 = "//span[@data-translate-key='2NdQuarterOddEven']"
+#qoe2_ot =
+#    qoe2_ov =
+#    qoe2_et =
+#    qoe2_ev =
+#qoe_3 = "//span[@data-translate-key='3RQuarterOddEven']"
+#    #    qoe3_ot =
+#    #    qoe3_ov =
+#    #    qoe3_et =
+#    #    qoe3_ev =
+#qoe_4 = "//span[@data-translate-key='4ThQuarterOddEven']"
+#     #   qoe4_ot =
+#     #   qoe4_ov =
+#    #  qoe4_et =
+#     #   qoe4_ev =
 
     #    types = "//div[@data-translate-market='Winner']"
     #    values = "//div[@class='outcome-pricedecimal']"
@@ -54,25 +55,21 @@ def test_web1():
         time.sleep(3)
         page.locator(highlights).click()
         time.sleep(3)
-        page.locator(event).click()
+        events = page.locator(event).locator_all()
+        #quarters = []
+        events.first.click()
+        
         time.sleep(3)
         page.locator(qtrs).click()
         time.sleep(3)
         page.locator(qoe_1).click()
         time.sleep(3)
-        
-        
-      #  for i in games:
-      #  game.click()
-      #  time.sleep(3)
-            
-        odd_t = page.locator(qoe1_ot)
-        odd_v = page.locator(qoe1_ov)
-        type = odd_t.inner_text()
-        value = odd_v.inner_text()
-            
-        print(type)
-        print(value)
+        qoe1_odds = {
+            'Q1 Odd': qoe1_ov.inner_text(),
+            'Q1 Even': qoe1_ev.inner_text()
+        }
+   
+        print(qoe1_odds)
         print("yeah motherfucker!!")
 
         browser.close()
