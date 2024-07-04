d73bc40d9ca1559e8333b9fa74d3cb9ed0c9fb35
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-07-04 11:20:28 +0200

    Update test_dpsa.py

--- test_dpsa.py
+++ test_dpsa.py
@@ -1,14 +1,20 @@
-import PyPDF2
+import time
+import requests
+from datetime import datetime
+from playwright.sync_api import sync_playwright
 
-# Open the PDF file
-with open('Northern Cape Government jobs.pdf', 'rb') as pdf_file:
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+screenshot_path = f"screenshot_{timestamp}.png"
+indeed = 'https://za.indeed.com/'
 
-# Extract text from all pages (modify the loop for specific pages)
-text = ''
-for page_num in range(len(pdf_reader.pages)):
-    page = pdf_reader.pages[page_num]
-    text += page.extractText()
-
-# Process or print the extracted text
-print(text)
+def test_web1():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://za.indeed.com/")
+        page.screenshot(path="screenshot.png", full_page=True)
+        #page.get_by_placeholder("Job title, keywords, or").fill("kill all leftists")        
+        #page.get_by_placeholder("City, state, zip code, or \"").fill("upington")
+        #page.get_by_role("button", name="Find jobs").click()
+        #page.goto("https://za.indeed.com/")
+        browser.close()
