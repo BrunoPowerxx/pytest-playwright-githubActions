d22fd0a8b8e549fec1315cd972cf5efe51ec87f0
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-07-05 08:59:55 +0200

    Update test_dpsa.py

--- test_dpsa.py
+++ test_dpsa.py
@@ -3,33 +3,21 @@ import time
 from datetime import datetime
 from playwright.sync_api import sync_playwright
 
-timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-screenshot_directory = "screenshots"  # New directory to store screenshots
-
-if not os.path.exists(screenshot_directory):
-    os.makedirs(screenshot_directory)
+#timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
 def test_web1():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://za.indeed.com/")
-        screenshot_path = os.path.join(screenshot_directory, f"screenshot_{timestamp}.png")
-        page.screenshot(path=screenshot_path, full_page=True)
+        page.screenshot(path='shot_one.png', full_page=True)
+        # highlight and fill job title input
+        page.screenshot(path='shot_two.png', full_page=True)
+        # highlight and fill location input
+        page.screenshot(path='shot_three.png', full_page=True)
         browser.close()
 
-        # Optionally, you can commit the screenshot to version control if needed
-        # Example of committing the screenshot file
-        commit_screenshot(screenshot_path)
-        
 
-def commit_screenshot(filepath):
-    # Perform git commands to add and commit the screenshot file
-    os.system(f"git add {filepath}")
-    os.system(f"git commit -m 'Added screenshot {os.path.basename(filepath)}'")
-    os.system("git push")
-
-# Run the test function
 
 
 
