d88403573f494adbfaee2c811e7fb0e60285ca12
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-07-10 11:26:29 +0200

    Create fml.py

---
+++ fml.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import streamlit_pandas as sp
+
+file = "/content/sample_data/california_housing_test.csv"
+df = pd.read_csv(file)
+
+all_widgets = sp.create_widgets(df)
+res = sp.filter_df(df, all_widgets)
+st.title("Streamlit AutoPandas")
+st.header("Original DataFrame")
+st.write(df)
+
+st.header("Result DataFrame")
+st.write(res)
