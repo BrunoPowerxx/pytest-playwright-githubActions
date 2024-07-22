1048e7c526767b0bcf07c7df790117377bad3c45
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-07-22 10:59:29 +0200

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -3,7 +3,16 @@ from selectors import *
 import time
 
 
+        # Wait for all elements matching 'div.row.eventRow'
+        
 
+        # Get all elements matching 'div.row.eventRow'
+        
+
+        # Process each event element
+        for element in event_elements:
+            # Example: Extract text from each event element
+            text = element.text_content()
 def test_web1():
     with sync_playwright() as p:
         browser = p.chromium.launch()
@@ -11,17 +20,19 @@ def test_web1():
         page.goto("https://betway.co.za/sport/basketball")
         time.sleep(3)
         page.screenshot(path='shot1.png', full_page=True)
-        events = page.query_selector_all("label[data_translate_type='event' and data_translate_set='Basketball']")
+        page.wait_for_selector('div.row.eventRow')
+        events = page.query_selector_all('div.row.eventRow')
         quarters = []
         for event in events:
             event.click()
-            qtrs = page.wait_for_selector('span[data-translate-key="Quarters"]')
-            qtrs.click()
-            time.sleep(3)
-            page.screenshot(path='shot2.png', full_page=True)
-            qoe1 = page.wait_for_selector('span[data-translate-key="1StQuarterOddEven"]')
-            qoe1.click()
-            page.screenshot(path='shot4.png', full_page=True)
+            print("event")
+            #qtrs = page.wait_for_selector('span[data-translate-key="Quarters"]')
+            #qtrs.click()
+            #time.sleep(3)
+            #page.screenshot(path='shot2.png', full_page=True)
+            #qoe1 = page.wait_for_selector('span[data-translate-key="1StQuarterOddEven"]')
+            #qoe1.click()
+            #page.screenshot(path='shot4.png', full_page=True)
             
             
         browser.close()
